0fe90347bb022f7e66aadbd458b949da7a90f4ae
gominzip <ahaminji@naver.com>
2025-01-26 03:14:37 +0900

    feat: change md to mdx and add remarkGfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,21 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact(), tailwind()],
+  integrations: [
+    preact(),
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "dracula" },
+      remarkPlugins: [remarkGfm],
+      remarkRehype: { footnoteLabel: "주석" },
+      gfm: false,
+    }),
+  ],
 });
